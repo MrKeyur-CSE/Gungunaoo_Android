aaa1a0575a1a4ef9e4ad93116f1fab7a429b35cc
MrKeyur-CSE <keyur.developer.cse@gmail.com>
2022-07-16 22:59:22 -0400

    Floating Action

--- app/src/main/java/com/example/gungunaoo_android/HomeScreen.java
+++ app/src/main/java/com/example/gungunaoo_android/HomeScreen.java
@@ -1,14 +1,47 @@
 package com.example.gungunaoo_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeScreen extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        navigationView = findViewById(R.id.user_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_container, new HomeFragment()).commit();
+        navigationView.setSelectedItemId(R.id.nav_home);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()){
+
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.nav_setting:
+                        fragment = new SettingFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_container, fragment).commit();
+                return true;
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/gungunaoo_android/MainActivity.java
+++ app/src/main/java/com/example/gungunaoo_android/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView notRegister_btn;
+    Button login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +20,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         notRegister_btn = (TextView) findViewById(R.id.notRegister_btn);
+        login_btn = findViewById(R.id.login_btn);
 
         notRegister_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, RegistrationScreen.class));
+            }
+        });
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, HomeScreen.class));
             }
         });
 
